7745b3ec943083260efae709aca254841e41e114
iljazimakovs <ilja930811@gmail.com>
2026-03-25 07:33:49 +1000

    update

--- src/components/sections/Portfolio.tsx
+++ src/components/sections/Portfolio.tsx
@@ -149,7 +149,7 @@ const projects: Project[] = [
     media: [
       {
         type: "image",
-        src: "https://hackster.imgix.net/uploads/attachments/1208398/_BLGl2VleYg.blob?auto=compress%2Cformat&w=900&h=675&fit=min",
+        src: "/images/_BLGl2VleYg.png",
       },
     ],
     hidden: false,
